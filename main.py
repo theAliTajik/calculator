e5cb30fc121cdd3841b96473c72dfafd8c38c430
Amirmasoudsh <142328391+Amirmasoudsh@users.noreply.github.com>
2023-08-15 22:45:57 +0330

    changed ui

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ frame_row2 = ttk.Frame()
 frame_row3 = ttk.Frame()
 frame_row4 = ttk.Frame()
 
+invincible = tk.StringVar()
+
 entry=ttk.Entry(Window,width=17,font="calibre 30")
 entry.pack(pady=10,)
 
@@ -44,21 +46,29 @@ button = ttk.Button(frame_row2, text="3", width=15)
 button.pack(side="left", padx=5, pady=5)
 frame_row2.pack(side="top", anchor="n")
 
+button = ttk.Button(frame_row3, text=".", width=15)
+button.pack(anchor="center", padx=5, pady=5)
+
+
 button = ttk.Button(frame_row3, text="0", width=15)
-button.pack(side="left", padx=5, pady=5)
-frame_row3.pack(side="top", anchor="n")
+button.pack(anchor="center", padx=5, pady=5)
+
+button = ttk.Button(frame_row3, text="=", width=15)
+button.pack(anchor="center", padx=5, pady=5)
+frame_row3.pack()
 
 button = ttk.Button(frame_row4, text="-", width=10)
-button.pack(side="left", padx=4, pady=5)
+button.pack(side="left", padx=5, pady=5)
+
 
 button = ttk.Button(frame_row4, text="×", width=10)
-button.pack(side="left", padx=4, pady=5)
+button.pack(side="left", padx=5, pady=5)
 
 button = ttk.Button(frame_row4, text="+", width=10)
-button.pack(side="left", padx=4, pady=5)
+button.pack(side="left", padx=5, pady=5)
 
 button = ttk.Button(frame_row4, text="÷", width=10)
-button.pack(side="left", padx=4, pady=5)
+button.pack(side="left", padx=5, pady=5)
 frame_row4.pack(side="top", anchor="n")
 
 Window.mainloop()
