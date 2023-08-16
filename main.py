f386be282ac3447ec9e7e4957626aca8667761d0
Ali Tajik <142308097+theAliTajik@users.noreply.github.com>
2023-08-16 13:30:57 +0330

    merged amir with main

--- main.py
+++ main.py
@@ -82,6 +82,7 @@ def buttonInput(num):
 # window
 Window = ttk.Window(themename="darkly")
 Window.title("calculator")
+Window.geometry("500x300")
 
 # tk var
 input = tk.StringVar()
@@ -92,9 +93,30 @@ frame_row1 = ttk.Frame()
 frame_row2 = ttk.Frame()
 frame_row3 = ttk.Frame()
 frame_row4 = ttk.Frame()
+frame_delete = ttk.Frame()
 
+<<<<<<< HEAD
 entry=ttk.Entry(Window,width=17,font="calibre 30", textvariable=input)
 entry.pack(pady=10)
+=======
+invincible = tk.StringVar()
+
+entry=ttk.Entry(Window,width=19,font="calibre 33")
+entry.pack(pady=10)
+
+button = ttk.Button(frame_delete, text="CE", width=15)
+button.pack(side="left", padx=5, pady=5)
+
+button = ttk.Button(frame_delete, text="C", width=15)
+button.pack(side="left", padx=5, pady=5)
+
+button = ttk.Button(frame_delete, text="Delete", width=15)
+button.pack(side="left", padx=5, pady=5)
+
+button = ttk.Button(frame_delete, text="÷", width=5)
+button.pack(side="left", padx=5, pady=5)
+frame_delete.pack(side="top", anchor="n")
+>>>>>>> origin/Amir
 
 button = ttk.Button(frame_row0, text="7", width=15, command=lambda: buttonInput(7))
 button.pack(side="left", padx=5, pady=5)
@@ -103,6 +125,9 @@ button = ttk.Button(frame_row0, text="8", width=15, command=lambda: buttonInput(
 button.pack(side="left", padx=5, pady=5)
 
 button = ttk.Button(frame_row0, text="9", width=15, command=lambda: buttonInput(9))
+button.pack(side="left", padx=5, pady=5)
+
+button = ttk.Button(frame_row0, text="×", width=5)
 button.pack(side="left", padx=5, pady=5)
 frame_row0.pack(side="top", anchor="n")
 
@@ -114,6 +139,9 @@ button.pack(side="left", padx=5, pady=5)
 
 button = ttk.Button(frame_row1, text="6", width=15, command=lambda: buttonInput(6))
 button.pack(side="left", padx=5, pady=5)
+
+button = ttk.Button(frame_row1, text="-", width=5)
+button.pack(side="left", padx=5, pady=5)
 frame_row1.pack(side="top", anchor="n")
 
 button = ttk.Button(frame_row2, text="1", width=15, command=lambda: buttonInput(1))
@@ -124,12 +152,29 @@ button.pack(side="left", padx=5, pady=5)
 
 button = ttk.Button(frame_row2, text="3", width=15, command=lambda: buttonInput(3))
 button.pack(side="left", padx=5, pady=5)
+
+button = ttk.Button(frame_row2, text="+", width=5)
+button.pack(side="left", padx=5, pady=5)
 frame_row2.pack(side="top", anchor="n")
 
+<<<<<<< HEAD
 button = ttk.Button(frame_row3, text="0", width=15, command=lambda: buttonInput(0))
+=======
+invic = ttk.Label(frame_row3,text="",width=18)
+invic.pack(side="left", padx=6, pady=3)
+
+button = ttk.Button(frame_row3, text="0", width=15)
+>>>>>>> origin/Amir
+button.pack(side="left", padx=5, pady=5)
+
+button = ttk.Button(frame_row3, text=".", width=15)
+button.pack(side="left", padx=5, pady=5)
+
+button = ttk.Button(frame_row3, text="=", width=5)
 button.pack(side="left", padx=5, pady=5)
 frame_row3.pack(side="top", anchor="n")
 
+<<<<<<< HEAD
 button = ttk.Button(frame_row4, text="-", width=10, command=lambda: buttonInput("-"))
 button.pack(side="left", padx=4, pady=5)
 
@@ -142,5 +187,8 @@ button.pack(side="left", padx=4, pady=5)
 button = ttk.Button(frame_row4, text="÷", width=10, command=lambda: buttonInput("÷"))
 button.pack(side="left", padx=4, pady=5)
 frame_row4.pack(side="top", anchor="n")
+=======
+
+>>>>>>> origin/Amir
 
 Window.mainloop()
